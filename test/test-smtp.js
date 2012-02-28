28352f852f5e4071016b2de26a3dc9a679953183
Lupo Montero <lupo@e-noise.com>
2012-02-28 14:51:32 -0500

    Made Horseshoe constructor inherit from EventEmitter and implemented basic events

--- test/test-smtp.js
+++ test/test-smtp.js
@@ -8,10 +8,7 @@ var
   // This assumes you have a file with horseshoe options in json format called
   // config.json in the directory above this script. This config file IS NOT
   // included in the repo so you should create your own.
-  config = JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'utf8')),
-  horseshoe = new Horseshoe(config);
-
-horseshoe.setTemplatesPath(__dirname + '/mail_templates/');
+  config = JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'utf8'));
 
 suite.addBatch({
 
@@ -27,7 +24,7 @@ suite.addBatch({
 
       horseshoe.send(msg, this.callback);
     },
-    'get sender address error': function (errors, success) {
+    'get errors array with sender address error': function (errors, success) {
       assert.ok(!success);
       assert.ok(util.isArray(errors));
       errors.forEach(function (er) {
@@ -36,6 +33,27 @@ suite.addBatch({
         assert.ok(er.name === 'HorseshoeError');
         assert.ok(er.email && er.email.to === 'lupo@e-noise.com');
       });
+    }
+  },
+
+  'send smtp error no sender (test data event)': {
+    topic: function () {
+      var
+        horseshoe = new Horseshoe({ transport: 'smtp' }),
+        msg = {
+          to: 'lupo@e-noise.com',
+          template: 'users-signup',
+          data: { user: { firstname: 'Lupo' } }
+        };
+
+      horseshoe.on('data', this.callback);
+      horseshoe.send(msg);
+    },
+    'get sender address error': function (er, success) {
+      assert.ok(er instanceof Error);
+      assert.ok(typeof er.message === 'string');
+      assert.ok(er.name === 'HorseshoeError');
+      assert.ok(er.email && er.email.to === 'lupo@e-noise.com');
     }
   },
 
@@ -66,17 +84,41 @@ suite.addBatch({
 
   'send smtp': {
     topic: function () {
-      var msg = {
-        to: 'lupo@e-noise.com',
-        template: 'users-signup',
-        data: { user: { firstname: 'Lupo' } }
-      };
+      var
+        horseshoe = new Horseshoe(config),
+        msg = {
+          to: 'lupo@e-noise.com',
+          template: 'users-signup',
+          data: { user: { firstname: 'Lupo' } }
+        };
 
+      horseshoe.setTemplatesPath(__dirname + '/mail_templates/');
       horseshoe.send(msg, this.callback);
     },
     'get success (this has sent real email, check mailbox)': function (errors, success) {
       assert.ok(success);
       assert.ok(!errors);
+    }
+  },
+
+  'send bad template': {
+    topic: function () {
+      var
+        horseshoe = new Horseshoe(config),
+        msg = {
+          to: 'lupo@e-noise.com',
+          template: 'bad-template',
+          data: { user: { firstname: 'Lupo' } }
+        };
+
+      horseshoe.setTemplatesPath(__dirname + '/mail_templates/');
+      horseshoe.send(msg, this.callback);
+    },
+    'get parse error': function (errors, success) {
+      assert.ok(util.isArray(errors));
+      errors.forEach(function (er) {
+        assert.ok(/^Parse error/.test(er.message));
+      });
     }
   }
 
