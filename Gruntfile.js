4991409226c13a71a4105925f57e69b2ee82bd3a
Lupo Montero <lupo@e-noise.com>
2014-02-25 18:45:10 -0500

    Removing reference to old bin file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
-      files: [ 'Gruntfile.js', 'index.js', 'bin/horseshoe', 'test/**/*.js' ]
+      files: [ 'Gruntfile.js', 'index.js', 'test/**/*.js' ]
     },
 
     nodeunit: {
